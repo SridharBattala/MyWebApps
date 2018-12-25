321a5e0c9f067867da5d9ef9811ad775c0673bb6
sridhar <sridhar@sridhars-mbp>
2018-12-24 18:03:20 -0800

    committing data structure programs

---
+++ leave/src/main/java/com/sree/leave/programs/datastructures/stack/Stack.java
@@ -0,0 +1,36 @@
+package com.sree.leave.programs.datastructures.stack;
+
+import java.util.ArrayList;
+
+public class Stack {
+	
+	private ArrayList list ;
+	private int top;
+	
+	
+	public Stack() {
+		// TODO Auto-generated constructor stub
+		list= new ArrayList();
+		top=-1;
+		
+	}
+	
+	public void push(Object data){
+		top++;
+		list.add(data);
+		
+	}
+	public Object pop(){
+		Object data= list.get(top);
+		list.remove(top);
+		top--;
+		return data;
+	}
+	
+	public boolean isEmpty(){
+		return (top == -1);
+		
+	}
+	
+
+}
---
+++ leave/src/main/java/com/sree/leave/programs/datastructures/stack/Student.java
@@ -0,0 +1,15 @@
+package com.sree.leave.programs.datastructures.stack;
+
+public class Student {
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAddress(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
